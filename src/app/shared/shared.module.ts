52f2767aad26e039dbe597c166c976edef7dd571
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-10-11 17:17:41 +0100

    query functionality added to shows

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { HttpClientModule } from '@angular/common/http'
   ],
   exports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ]
 })
 export class SharedModule { }
--- src/app/shows/showComponent/show.component.ts
+++ src/app/shows/showComponent/show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { FetchShowService } from 'src/app/services/fetch-show.service';
 import { Show } from 'src/app/type/Show';
@@ -9,20 +10,55 @@ import { Show } from 'src/app/type/Show';
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit, OnDestroy {
+  public filteredShows!: Show[];
   public shows: Show[]=[];
   private subscription! : Subscription
-  constructor(private showService: FetchShowService) { }
+  private _filterBy: string = ''
+
+  constructor(private showService: FetchShowService, private route: Router) { }
 
   ngOnInit(): void {
    this.subscription = this.showService.fetchShows().subscribe(
      (shows: Show[])=>{
         this.shows.push(...shows);
+        this.filteredShows = this.shows;
      }
-   )
+   );
+
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+
+  public get filterBy() : string {
+    return this._filterBy
+  }
+
+  public set filterBy(filter : string) {
+    this._filterBy = filter;
+    this.performFilter();
+    //console.log(JSON.stringify(this.filteredShows));
+  }
+
+  private performFilter(){
+    if(this._filterBy !==''){
+      this.filteredShows = this.shows.filter((show:Show,index:number)=>{
+        return show.title.toLocaleLowerCase().search(this.filterBy.toLocaleLowerCase())>-1;
+      });
+    }else{
+      this.filteredShows = this.shows;
+    }
+  }
+
+  public onSelected(): void{
+    this.route.navigateByUrl('/film')
+    //console.log('selected film');
+
+  }
+
+
+
 
 }
